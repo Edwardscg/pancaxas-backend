0cf54f495a2c4b4cf1123f02ca0418591a1cccf8
Edward <edwardcornegomez@gmail.com>
2026-08-27 18:09:33 -0500

    Configuración inicial del backend

---
+++ pancaxas-backend/src/main/java/com/pancaxas/backend/PancaxasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pancaxas.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PancaxasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PancaxasBackendApplication.class, args);
+    }
+
+}
---
+++ pancaxas-backend/src/test/java/com/pancaxas/backend/PancaxasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pancaxas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancaxasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
